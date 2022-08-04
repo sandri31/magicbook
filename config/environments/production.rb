23112b67008bf21677adb442b8feac021b199590
florent sandri <sandri822@hotmail.fr>
2022-08-04 08:21:46 +0200

    📝 Added perform_deleveries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "lerepairedebolas.herokuapp.com", protocol: "https" }
-
-  config.action_mailer.perform_deleveries = false
+  config.action_mailer.perform_deleveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',

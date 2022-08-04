fcca3172f8342ccc114098d7e89188b301479f65
florent sandri <sandri822@hotmail.fr>
2022-08-04 08:09:42 +0200

    📝 Added modification mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,18 +5,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'localhost:3000',
-  user_name:            ENV['SMTP_USER_NAME'],
-  password:             ENV['SMTP_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true
-  }
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time

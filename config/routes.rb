c458ceb8a13af353802034da5124442359bfb150
florent sandri <sandri822@hotmail.fr>
2022-11-15 08:07:09 +0100

    ✏️ Fix typos

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Public pages
   root 'public#index'
   get 'about', to: 'public#about'
-  get 'terms', to: 'public#terms'
+  # get 'terms', to: 'public#terms'
   get 'random', to: 'public#random'
   get 'construction', to: 'public#construction'
 end

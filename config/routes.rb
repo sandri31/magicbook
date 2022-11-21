3939055f6885a8309ebb993cb0024a60457f3338
florent sandri <sandri822@hotmail.fr>
2022-11-21 10:12:41 +0100

    📝 Update route

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,5 @@ Rails.application.routes.draw do
 
   # Public pages
   root 'public#index'
-  get 'about', to: 'public#about'
-  # get 'terms', to: 'public#terms'
   get 'random', to: 'public#random'
-  get 'construction', to: 'public#construction'
 end

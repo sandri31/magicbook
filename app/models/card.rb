cf8659d2db7c44cb5fa8a57ae1546b9070986adf
Symitsh <sandri822@hotmail.fr>
2023-01-17 19:45:26 +0100

    📝 Update card.rb

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,14 @@
 
 class Card < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :user_id, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :printed_name, presence: true, uniqueness: { scope: :user_id }
   delegate :total_price, to: :cards, prefix: true
+
   scope :white, -> { where(color_identity: 'W') }
   scope :blue, -> { where(color_identity: 'U') }
   scope :black, -> { where(color_identity: 'B') }
@@ -13,11 +20,6 @@ class Card < ApplicationRecord
                        where.not(color_identity: 'W').where.not(color_identity: 'U').where.not(color_identity: 'B')
                             .where.not(color_identity: 'R').where.not(color_identity: 'G').where.not(color_identity: '')
                      }
-  validates :name, presence: true
-  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
-  validates :user_id, presence: true
-  validates :name, presence: true, uniqueness: { scope: :user_id }
-  validates :printed_name, presence: true, uniqueness: { scope: :user_id }
 
   def total_price
     price * quantity

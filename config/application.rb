755e4c43ea8052b428fe19c9b5c09dbc52a8fd87
Symitsh <sandri822@hotmail.fr>
2023-01-24 17:41:35 +0100

    🔧 Change config file

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Magicbook
     config.load_defaults 7.0
 
     # Set the new domain name as the base URL for the app
-    config.action_controller.default_url_options = { host: 'repairemagic.fr' }
+    config.action_controller.default_url_options = { host: ENV['HOSTNAME'] }
 
     config.i18n.available_locales = %i[en fr]
 

bb86bc8318361135b036b5fa2478497950c05435
Symitsh <sandri822@hotmail.fr>
2023-01-24 15:19:04 +0100

    🔧 Change config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'www.repairemagic.fr' }
-  config.hosts << ['www.repairemagic.fr']
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
+  config.hosts << ENV['HOSTNAME']
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {

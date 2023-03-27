bc2fcb4bf4cb4db8365348200c7ff626147ec32e
Florent S <sandri822@hotmail.fr>
2023-03-27 11:40:26 +0200

    🔧 Update configuration file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'www.repairemagic.fr' }
   config.hosts << 'www.repairemagic.fr'
-  config.hosts << 'repairemagic.fr'
+  # config.hosts << 'repairemagic.fr'
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {

b849cdfa75f49134b7441a758ae0daa84eb6bee0
Florent S <sandri822@hotmail.fr>
2023-03-27 16:35:33 +0200

    🔧 Update config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'repairemagic.fr' }
+  config.action_mailer.default_url_options = { host: 'repairemagic.fr', skip_relative_url_root: true }
 
   config.action_mailer.delivery_method = :smtp
   config.hosts << 'www.repairemagic.fr'

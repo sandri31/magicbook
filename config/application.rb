d3bfb00a65befef130333e025023f6b926934567
Florent S <sandri822@hotmail.fr>
2023-03-30 15:39:17 +0200

    add path to file allowed_hosts

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative '../lib/allowed_hosts'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,7 +24,7 @@ module Magicbook
 
     # Autoload the custom middleware
     config.autoload_once_paths << Rails.root.join('lib')
-    config.middleware.use "AllowedHosts"
+    config.middleware.use AllowedHosts
 
     # Configuration for the application, engines, and railties goes here.
     #

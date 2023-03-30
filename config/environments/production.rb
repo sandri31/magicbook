b7e9e7943ed01ee1afb38156462fb0b8bdb9f99b
Florent S <sandri822@hotmail.fr>
2023-03-30 15:00:30 +0200

    🔧 change environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
 
-  config.hosts << ENV['HOSTNAME']
-  config.hosts << ENV['ALIAS_HOSTNAME']
+  config.hosts << /\.repairemagic\.fr\z/
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
